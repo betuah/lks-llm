7da98fe4c99cc739b7f2a90bc47d19d3041842cb
betuah <betuahanugerah@gmail.com>
2024-08-12 15:43:51 +0700

    feat(landing): add button to signin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+'use client';
+
 import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
     <div className="flex items-center justify-center h-screen w-full text-center">
       <div className="flex flex-col p-5 bg-accent shadow-md shadow-accent rounded-lg border border-primary w-full max-w-[400px]">
         <span className="font-bold text-[23px] text-primary">Hello, Congratulations ! 🥳 </span>
         <span className="text-[13px]">If you see this, your app is working.</span>
         <div className="flex mt-5 mb-2">
-          <Button variant={"default"} className="text-[13px] h-8 mx-auto">Sign In Here</Button>
+          <Button variant={"default"} className="text-[13px] h-8 mx-auto" onClick={() => router.push("/signin")}>Sign In Here</Button>
         </div>
       </div>
     </div>
